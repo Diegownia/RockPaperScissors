0ddc33bfe5464b1345b9ccb750c722c511da0f76
Diegownia <Pkorczak93@gmail.com>
2022-08-11 14:22:37 +0100

    QuickDraw begining

---
+++ Games.Data.Core/QuickDrawModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games.Data.Core
+{
+    public class QuickDrawModel
+    {
+        public DateTime ReactionTime { get; set; }
+        public DateTime RoundTime { get; set; }
+
+
+    }
+}
---
+++ QuickDraw/QuickDraw.cs
@@ -0,0 +1,26 @@
+﻿using Games.Data.Core;
+
+public class QuickDraw
+{
+
+    private IGameManager _gameManager;
+    private IDrawer _drawer;
+
+    public QuickDraw(IGameManager gameManager, IDrawer drawer)
+    {
+        _gameManager = gameManager;
+        _drawer = drawer;
+    }
+
+    public void Game()
+    {
+        Console.WriteLine("Welcome to the Quick Draw");
+        Console.WriteLine("Your reflex matters!");
+
+    }
+
+    private void GameLoop()
+    {
+
+    }
+}
